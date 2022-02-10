368fa9f278402e20d55cd58b7365cc27f6295e16
luiseer <85654418+luiseer@users.noreply.github.com>
2022-02-09 21:38:58 -0600

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,15 @@ const NavBar = () => {
         <div>
             <nav className='flex justify-around mt-5 bg-rosa-principal'>
                 <Link to="/shop">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-home"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="#000000"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <polyline points="5 12 3 12 12 3 21 12 19 12" />
                         <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
@@ -19,7 +27,15 @@ const NavBar = () => {
                     </svg>
                 </Link>
                 <Link to="/cart">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        class="icon icon-tabler icon-tabler-shopping-cart"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="#000000" fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <circle cx="6" cy="19" r="2" />
                         <circle cx="17" cy="19" r="2" />
